5b8a977958473d69fe458ec226d0217592bfbd28
Ariaj Sarkar <rajsarkar02205@gmail.com>
2026-05-28 01:07:32 +0530

    feat: update version to 3.1.1 across package files

--- src/lib/AppMeta.ts
+++ src/lib/AppMeta.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "A4Chat";
-export const APP_VERSION = "3.1.0";
+export const APP_VERSION = "3.1.1";
 export const GITHUB_URL = "https://github.com/AriajSarkar/A4Chat";
 export const GITHUB_RELEASES_URL = `${GITHUB_URL}/releases`;
